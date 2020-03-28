e36246322988e965b179f81bf6a2ffc071837825
Sam2303 <sjp2000@live.com>
2020-03-28 16:48:33 +0000

    fixed issues where the css/js files werent being accessed+

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require ("express");
+const path = require ("path");
+
 const app = express();
-app.use(express.static("src"));
+
+
+app.use(express.static("src/pages"));
+app.use(express.static(path.join(__dirname, 'src')));
+
 app.listen(8080);
+console.log("Website running on: http://127.0.0.1:8080")

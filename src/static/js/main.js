79baaf1846c831085ad4845fea2f65fbc3a62194
Sam2303 <sjp2000@live.com>
2020-03-28 16:31:31 +0000

    made a start on the home and nav bar

---
+++ src/static/js/main.js
@@ -0,0 +1,6 @@
+let mainNav = document.getElementById('menu');
+let navBarToggle = document.getElementById('navBarToggle');
+
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active');
+});
